db146178f21f6b37691387a99cbd0b21cd610578
Yuriy Nikolaev <warlokk83@gmail.com>
2021-04-02 20:45:25 +0400

    Moved plusDays to DataGenerator, array for cities added

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -3,24 +3,42 @@ package ru.netology.data;
 import com.github.javafaker.Faker;
 
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
     private DataGenerator() {
+    }
+
+    public static String plusDays(int n) {
+        LocalDate date = LocalDate.now();
+        String newDate = date.plusDays(n).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return newDate;
+    }
+
+    public static String city() {
+        String[] cities = {"Москва", "Кемерово", "Смоленск", "Тамбов", "Пенза", "Липецк", "Петрозаводск", "Астрахань", "Саратов", "Санкт-Петербург"};
+        return cities[new Random().nextInt(cities.length)];
+
     }
 
     public static class DeliveryRequest {
         public DeliveryRequest() {
         }
 
+
         public static DeliveryInfo clientInfo(String locale) {
             Faker faker = new Faker(new Locale(locale));
             return new DeliveryInfo(
                     faker.name().lastName().replace("ё", "е") + " " + faker.name().firstName().replace("ё", "е"),
-                    faker.address().city(),
+                    city(),
                     faker.phoneNumber().phoneNumber()
             );
         }
+
+
     }
 
 }
--- src/test/java/ru/netology/test/DeliveryTest.java
+++ src/test/java/ru/netology/test/DeliveryTest.java
@@ -1,13 +1,12 @@
-package ru.netology;
+package ru.netology.test;
 
-import com.github.javafaker.Bool;
 import org.junit.jupiter.api.*;
+import ru.netology.data.DataGenerator;
 import ru.netology.data.DataGenerator.*;
 import ru.netology.data.DeliveryInfo;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -19,41 +18,24 @@ public class DeliveryTest {
     private DeliveryInfo client = request.clientInfo("ru");
 
 
-    private LocalDate plusDays(int n) {
-        LocalDate date = LocalDate.now();
-        date = date.plusDays(n);
-        return date;
-    }
-
-
     @Test
     void shouldRescheduleAndSend() {
         open("http://localhost:9999");
         $$("[type=text]").first().setValue(client.getCity());
         $("[placeholder='Дата встречи']").sendKeys(CONTROL + "a", DELETE);
-        String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String newDate = DataGenerator.plusDays(4);
         $("[placeholder='Дата встречи']").setValue(newDate);
         $("[data-test-id=name] [type=text]").setValue(client.getName());
         $("[name=phone]").setValue(client.getPhone());
         $(".checkbox__box").click();
         $("div.form-field>[type=button]").submit();
-        Boolean incorrectCity = $(withText("недоступна")).isDisplayed();
-        if (incorrectCity) {
-            $$("[type=text]").first().sendKeys(CONTROL + "a", DELETE);
-            $$("[type=text]").first().setValue("Москва");
-            $("div.form-field>[type=button]").submit();
-        }
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + newDate));
         closeWindow();
 
         open("http://localhost:9999");
-        if (incorrectCity) {
-            $$("[type=text]").first().setValue("Москва");
-        } else {
-            $$("[type=text]").first().setValue(client.getCity());
-        }
+        $$("[type=text]").first().setValue(client.getCity());
         $("[placeholder='Дата встречи']").sendKeys(CONTROL + "a", DELETE);
-        newDate = plusDays(8).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        newDate = DataGenerator.plusDays(8);
         $("[placeholder='Дата встречи']").setValue(newDate);
         $("[data-test-id=name] [type=text]").setValue(client.getName());
         $("[name=phone]").setValue(client.getPhone());
@@ -61,7 +43,8 @@ public class DeliveryTest {
         $("div.form-field>[type=button]").submit();
         $(withText("Необходимо")).shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content>[type=button]").click();
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно запланирована на " + newDate));
+
     }
 
 
